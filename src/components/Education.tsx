aae6823ad61d77557feb05ae559e1405734e93dd
abhimanyu <manyu@unc.edu>
2025-02-01 00:12:35 -0500

    Update Education.tsx

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -3,12 +3,12 @@ const Education = () => {
   return (
     <section className="py-24 bg-white px-4 flex items-center justify-center" id="education">
       <div className="border-l-4 border-carolina-blue pl-6 max-w-2xl">
-        <h3 className="text-2xl font-bold text-unc-navy">B.S. Statistics/Operations Research, expected 2026</h3>
-        <p className="text-carolina-blue text-lg mt-2">Computer Science Minor, Parr Center for Ethics Member</p>
-        <p className="text-gray-600 mt-1">University of North Carolina at Chapel Hill</p>
+        <h3 className="text-2xl font-bold text-unc-navy">B.S. statistics/operations research, expected 2026</h3>
+        <p className="text-carolina-blue text-lg mt-2">compsci minor, parr center for ethics member</p>
+        // <p className="text-gray-600 mt-1">University of North Carolina at Chapel Hill</p> 
         <ul className="mt-4 text-gray-700 list-disc pl-4 space-y-2">
-          <li>Major GPA: [Your GPA]</li>
-          <li>Key Courses: [Course 1], [Course 2], [Course 3]</li>
+          <li><b>gpa</b>: 3.75, dean's list</li>
+          <li>key Courses: , [Course 2], [Course 3]</li>
         </ul>
       </div>
     </section>
