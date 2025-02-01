3152f81338136ec94ea53fe25b8b233c879e1035
abhimanyu <manyu@unc.edu>
2025-02-01 00:20:10 -0500

    Update Education.tsx

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -5,7 +5,9 @@ const Education = () => {
       <div className="border-l-4 border-carolina-blue pl-6 max-w-2xl">
         <h3 className="text-2xl font-bold text-unc-navy">B.S. statistics/operations research, expected 2026</h3>
         <p className="text-carolina-blue text-lg mt-2">compsci minor, parr center for ethics member</p>
-        // <p className="text-gray-600 mt-1">University of North Carolina at Chapel Hill</p> 
+        /*
+        <p className="text-gray-600 mt-1">University of North Carolina at Chapel Hill</p> 
+        */
         <ul className="mt-4 text-gray-700 list-disc pl-4 space-y-2">
           <li><b>gpa</b>: 3.75, dean's list</li>
           <li>key Courses: , [Course 2], [Course 3]</li>
