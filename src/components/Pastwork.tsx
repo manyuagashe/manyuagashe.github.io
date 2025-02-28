b74c304fab45f0b71babf69acc5ef8b61048068b
abhimanyu <manyu@unc.edu>
2025-02-28 16:20:32 -0500

    Update Pastwork.tsx

--- src/components/Pastwork.tsx
+++ src/components/Pastwork.tsx
@@ -1,7 +1,9 @@
 const Pastwork = () => {
   return (
+
+    <h2 className="text-2xl font-bold text-unc-navy">work exp</h2>
     <section className="py-24 bg-white px-4 flex items-center justify-center" id="Pastwork">
-      <h2 className="text-2xl font-bold text-unc-navy">work exp</h2>
+      
       <div className="border-l-4 border-carolina-blue pl-6 max-w-2xl">
         <h3 className="text-2xl font-bold text-unc-navy">student research intern</h3>
         <p className="text-carolina-blue text-lg mt-2">UNC School of Data Science and Society</p>
